15c25c5464f8e1e074c1a443f8b4b1320d4a02ec
Blake Johnson <blake41@gmail.com>
2018-04-07 19:37:19 -0400

    initial commit, auction contract working and everything deployable but no UI for auction

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,4 +1,5 @@
 const GradientToken = artifacts.require("GradientToken");
+const TokenAuction = artifacts.require("TokenAuction");
 const util = require("util");
 const fs = require("fs");
 const path = require("path");
@@ -9,6 +10,8 @@ module.exports = async function(deployer) {
   const addresses = {
     tokenAddress: GradientToken.address
   };
+
+  deployer.deploy(TokenAuction, tokenAddress)
 
   await writeFile(
     path.join(__dirname, "..", "front", "src", "addresses.json"),
